6638aa519d97d6df11d2fda1ec6b7115e4dfa000
Xtreme2K <satriadinatagg@gmail.com>
2024-11-07 09:03:21 +0800

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman Home</title></title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman Home</h1>
+    <img src="img/blackrose.jpg" alt="dani" width="200" >
+    <script src="js/script.js"></script>
+</body>
+</html>
